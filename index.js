ecd931147f569ae665d2bd4138af2e14b32a8c21
salfonsoMB17 <serggios17@gmail.com>
2026-05-30 20:26:00 -0500

    Exercise 4.2: Refactor application into separate modules

--- index.js
+++ index.js
@@ -1,52 +1,7 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-morgan.token('data', function getData (req) {
-  if (req.method === 'POST'){
-    return JSON.stringify(req.body)
-  }
-})
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
