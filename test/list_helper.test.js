c4922776186cde567b4ace75dae03ace4e8bdef1
salfonsoMB17 <serggios17@gmail.com>
2026-06-06 16:48:56 -0500

    Exercise 4.4: Add totalLikes helper function and tests

--- test/list_helper.test.js
+++ test/list_helper.test.js
@@ -4,7 +4,36 @@ const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
   const blogs = []
-
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+    const listWithOneBlog = [
+        {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+        }
+    ]
+
+    test('of empty list is zero', () => {
+        assert.strictEqual(listHelper.totalLikes([]), 0)
+    })
+
+    test('when list has only one blog equals the likes of that', () => {
+        assert.strictEqual(listHelper.totalLikes(listWithOneBlog), 5)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const blogs = [
+        { title: 'A', author: 'A', likes: 3 },
+        { title: 'B', author: 'B', likes: 7 },
+        { title: 'C', author: 'C', likes: 2 }
+        ]
+        assert.strictEqual(listHelper.totalLikes(blogs), 12)
+    })
 })
