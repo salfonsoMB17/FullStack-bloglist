09f15fde987d6bc536cbafe7514e0291feba5623
salfonsoMB17 <serggios17@gmail.com>
2026-06-13 19:37:38 -0500

    Exercise 4.9: Verify blog id property and configure toJSON transform

--- test/blog_api.test.js
+++ test/blog_api.test.js
@@ -12,7 +12,19 @@ test('blogs are returned as json', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/)
     
-  assert.strictEqual(response.body.length, 0)
+  assert.strictEqual(response.body.length, 1)
+})
+
+test('blogs have id property defined', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  // Validar el campo en cada registro
+  response.body.forEach(blog => {
+  assert(blog.id)
+}) 
 })
 
 after(async () => {
