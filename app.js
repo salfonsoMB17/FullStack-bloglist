e81efb8893e7ef734ffd439bdfb52ab4af18c13e
salfonsoMB17 <serggios17@gmail.com>
2026-07-11 17:02:13 -0500

    Exercise 4.18: Implement token-based authentication with login endpoint

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const morgan = require('morgan')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
@@ -17,5 +18,6 @@ app.use(express.json())
 app.use(morgan('tiny'))
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
