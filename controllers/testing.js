7a13e4316b1a4851132b75adeef87f496df0d703
salfonsoMB17 <serggios17@gmail.com>
2026-08-22 16:08:43 -0500

    Exercise 5.18: Add testing reset endpoint

---
+++ controllers/testing.js
@@ -0,0 +1,11 @@
+const testingRouter  = require('express').Router()
+const Blog = require('../models/blogs')
+const User = require('../models/users')
+
+testingRouter.post('/', async (request, response) => {
+  await Blog.deleteMany({})
+  await User.deleteMany({})
+  response.status(204).end()
+})
+
+module.exports = testingRouter
