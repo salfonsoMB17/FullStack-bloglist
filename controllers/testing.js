f981c58d1b99bbc7240194f2b47279d0c23b7a4a
salfonsoMB17 <serggios17@gmail.com>
2026-08-22 19:47:39 -0500

    Add /api/testing/reset endpoint for E2E tests

--- controllers/testing.js
+++ controllers/testing.js
@@ -2,7 +2,7 @@ const testingRouter  = require('express').Router()
 const Blog = require('../models/blogs')
 const User = require('../models/users')
 
-testingRouter.post('/', async (request, response) => {
+testingRouter.post('/reset', async (request, response) => {
   await Blog.deleteMany({})
   await User.deleteMany({})
   response.status(204).end()
